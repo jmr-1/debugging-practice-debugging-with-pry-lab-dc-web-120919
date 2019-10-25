ec0a5498f1db4f0a202beaf5e7e54132b58665a0
J <jmrumingan@gmail.com>
2019-10-25 17:15:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -11,8 +11,10 @@ def crew
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
-def greet_crew (crew_member)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+def greet_crew (crew)
+  new_crew = []
+  crew.each {|crew_member| new_crew << "Hello #{crew_member}."}
+  return new_crew
 end
 
 def engage
