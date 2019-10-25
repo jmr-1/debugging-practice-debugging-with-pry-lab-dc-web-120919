bbf6b9483fed6032470adbbeabcc456ed836f5f3
J <jmrumingan@gmail.com>
2019-10-25 16:30:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -12,6 +12,6 @@ class GasStation
 end
 
 petrol_petes = GasStation.new("Petrol Pete's", 50)
-binding.pry
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+binding.pry 
